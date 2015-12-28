0920933d71ef2a0e701a844afa20f4601730dace
powerboat9 <powerboat9@users.noreply.github.com>
2015-12-28 11:32:04 -0500

    Undid my last change.

--- downloader.lua
+++ downloader.lua
@@ -13,7 +13,7 @@ function getFileDownloadURLs(url, gatheredFiles, gatheredDirectories)
     local json = assert(handle.readAll(), "Reading failed for url " .. url)
     json = json:gsub("%s*\n%s*", "") --removes white space around '\n' and '\n'
     json = json:gsub("\"([^\"]*)\"%s*:%s*", "%1 = ") --turns '"hi": ' into 'hi = '
-    --json = json:sub(2, #json - 1) --removes brackets around the almostJSON
+    json = json:sub(2, #json - 1) --removes brackets around the almostJSON
     local jsonTable = assert(textutils.unserialize(json), "Failed to unserialize:\n" .. json)
     local files = gatheredUrls or {}
     local directories = gatheredDirectories or {}
