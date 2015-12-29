1c07c1d557a8b53e7fffba2abe0239d0ba0cd0de
powerboat9 <powerboat9@users.noreply.github.com>
2015-12-29 09:53:16 -0500

    Added debug code.

--- downloader.lua
+++ downloader.lua
@@ -43,6 +43,7 @@ for k, v in ipairs(getFileDownloadURLs(URL)) do
     local writeFile = fs.open(repository .. "/" .. v.path, "w")
     local webHandle = assert(http.get(v.url), "Getting " .. v.url .. " failed")
     local webContents = assert(webHandle.readAll(), "Reading " .. v.url .. " failed")
+    print(webContents) --REMOVE WHEN WORKS
     writeFile.write(webContents)
     writeFile.close()
 end
