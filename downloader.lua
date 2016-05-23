cb64b5a15c27b0f63337cca2f7f755206e666996
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-23 07:09:15 -0400

    Update downloader.lua

--- downloader.lua
+++ downloader.lua
@@ -1,17 +1,20 @@
 local user, repo, savePath, gitPath, branch = ...
 
-local baseURL = ("https://api.github.com/repos/%s/%s/contents/"):format(user, repo) .. "%s" .. (branch and ("?ref=" .. branch)) or ""
-local function getURL(path)
-    return baseURL:format(path)
+local function getBaseURL(path)
+    path = fs.combine(gitPath, path)
+    return baseURL = ("https://api.github.com/repos/%s/%s/contents/%s"):format(user, repo, path) .. (branch and ("?ref=" .. branch)) or ""
 end
 
 local downloads = {}
-local function download(path)
-    local gPath = fs.combine(gitPath, path)
+local function download(path, type)
     local sPath = fs.combine(savePath, path)
     local downURL = getURL(gPath)
-    downloads[downURL] = sPath
+    downloads[downURL] = {type = type, path = sPath}
     http.request(downURL)
 end
 
-local save = coroutine.create("
+local save = coroutine.create(function()
+    while true do
+        local _, url, h = os.pullEvent("http_success")
+        if downloads[url] then
+            if downloads
